1a453998fc8e1ff10d42ff965b4f96a0ccd0fd7f
D6_AkshayDahake_69695 <akshaydahake048@gmail.com>
2026-09-16 06:33:48 +0530

    new commit

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,21 +1,55 @@
 import api from "./api";
 
+// export async function createOrder(items) {
+//   const response = await api.post("/api/orders", { items });
+//   return response.data;
+// }
+
+// export async function getOrders() {
+//   const response = await api.get("/api/orders");
+//   return response.data;
+// }
+
+// export async function getOrderById(id) {
+//   const response = await api.get(`/api/orders/${id}`);
+//   return response.data;
+// }
+
+// export async function cancelOrder(id) {
+//   const response = await api.patch(`/api/orders/${id}/cancel`);
+//   return response.data;
+// }
+
+
+import axios from "axios";
+
+const ORDER_SERVICE_URL = "http://localhost:8083";
+
 export async function createOrder(items) {
-  const response = await api.post("/api/orders", { items });
+  const response = await axios.post(`${ORDER_SERVICE_URL}/api/orders`, {
+    items
+  });
+
   return response.data;
 }
 
 export async function getOrders() {
-  const response = await api.get("/api/orders");
+  const response = await axios.get(`${ORDER_SERVICE_URL}/api/orders`);
   return response.data;
 }
 
 export async function getOrderById(id) {
-  const response = await api.get(`/api/orders/${id}`);
+  const response = await axios.get(
+    `${ORDER_SERVICE_URL}/api/orders/${id}`
+  );
+
   return response.data;
 }
 
 export async function cancelOrder(id) {
-  const response = await api.patch(`/api/orders/${id}/cancel`);
+  const response = await axios.patch(
+    `${ORDER_SERVICE_URL}/api/orders/${id}/cancel`
+  );
+
   return response.data;
 }
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,11 +1,27 @@
 import api from "./api";
 
+// export async function getProducts() {
+//   const response = await api.get("/api/products");
+//   return response.data;
+// }
+
+// export async function getProductById(id) {
+//   const response = await api.get(`/api/products/${id}`);
+//   return response.data;
+// }
+
+import axios from "axios";
+
+const PRODUCT_SERVICE_URL = "http://localhost:8082";
+
 export async function getProducts() {
-  const response = await api.get("/api/products");
+  const response = await axios.get(`${PRODUCT_SERVICE_URL}/api/products`);
   return response.data;
 }
 
 export async function getProductById(id) {
-  const response = await api.get(`/api/products/${id}`);
+  const response = await axios.get(
+    `${PRODUCT_SERVICE_URL}/api/products/${id}`
+  );
   return response.data;
 }
